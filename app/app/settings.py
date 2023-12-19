277ef11d4a62e561574d2cfb148c8c99a3c53f27
Andrii Zabolotin <fenics7511@gmail.com>
2023-12-19 15:34:53 +0200

    Configurated User Model

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "user.apps.UserConfig",
+    "phonenumber_field",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "user.User"
